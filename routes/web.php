5781eaa78571e77376d21104dbdd7d27d43d2cf4
Javi Alcoba <javi.alcobanavero@alum.uca.es.es>
2026-05-05 19:59:38 +0200

    Página Formulario puesta en Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-Route::get('/', function () { return view('welcome'); });
-
-
-
 // Para usar un metodo de una clase hay que añadirlo primero:
+
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\FormularioController;
+use App\Http\Controllers\IndexController;
+
+/*
+Route::get('/', function () { 
+    return view('welcome'); 
+});
+*/
 
 // El primer parametro es la ruta a la que se accede (/perfil) y luego la clase (UserController) y el metodo (mostrar) de la clase al que accedemos
+Route::get('/', [IndexController::class, 'index'])->name('index');
+Route::get('/formulario', [FormularioController::class, 'index'])->name('formulario');
 Route::get('/perfil', [UserController::class, 'mostrar']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
