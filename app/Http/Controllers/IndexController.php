5ef4f36d7fafd00ff87ea26e7e22faf58c4e5c68
Claudia <claudi.garciurba@alum.uca.es>
2026-05-12 00:05:26 +0200

    Index estático

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Animal;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('index');
+        // 1. Cogemos todos los animales de la base de datos
+        // $animales = Animal::all(); 
+
+        // 2. Enviamos la variable a la vista 'index'
+        // return view('index', compact('animales'));
+
+        return view("index");
     }
 }
