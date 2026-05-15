3b50e4fd1ee000ddbd721b646dbf3bc01c86b41b
Claudia <claudi.garciurba@alum.uca.es>
2026-05-15 16:28:02 +0200

    Paginación animales

--- resources/views/vendor/pagination/simple-default.blade.php
+++ resources/views/vendor/pagination/simple-default.blade.php
@@ -1,57 +1,119 @@
 @if ($paginator->hasPages())
     <nav class="Paginacion-Navegacion">
-        {{-- Bloque de flechas: Previous ....... Next --}}
+        {{-- Bloque de botones superiores --}}
         <div class="Paginacion-Botones">
-            {{-- Botón Anterior --}}
             @if ($paginator->onFirstPage())
-                <span class="btn-pag-disabled">&laquo; Previous</span>
+                <p class="btn-pag-disabled">&laquo; Previous</p>
             @else
                 <a href="{{ $paginator->previousPageUrl() }}" class="btn-pag-link" rel="prev">&laquo; Previous</a>
             @endif
 
-            <span class="Paginacion-Separador">.......</span>
+            <div class="Separador-Vacio"></div>
 
-            {{-- Botón Siguiente --}}
             @if ($paginator->hasMorePages())
                 <a href="{{ $paginator->nextPageUrl() }}" class="btn-pag-link" rel="next">Next &raquo;</a>
             @else
-                <span class="btn-pag-disabled">Next &raquo;</span>
+                <p class="btn-pag-disabled">Next &raquo;</p>
             @endif
         </div>
 
-        {{-- Bloque de contador: 1...59 --}}
+        {{-- Bloque de contador inferior --}}
         <div class="Paginacion-Contador">
-            <p>
-                {{ $paginator->currentPage() }}...{{ $paginator->lastPage() }}
-            </p>
+            <div class="Contador-Flex">
+                {{-- Página 1 --}}
+                @if ($paginator->currentPage() == 1)
+                    <p class="cont-actual">1</p>
+                @else
+                    <a href="{{ $paginator->url(1) }}" class="cont-link">1</a>
+                @endif
+
+                {{-- Puntos izquierda --}}
+                @if ($paginator->currentPage() > 2)
+                    <p class="cont-puntos">. . .</p>
+                @endif
+
+                {{-- Página Actual --}}
+                @if ($paginator->currentPage() !== 1 && $paginator->currentPage() !== $paginator->lastPage())
+                    <p class="cont-actual">{{ $paginator->currentPage() }}</p>
+                @endif
+
+                {{-- Puntos derecha --}}
+                @if ($paginator->currentPage() < $paginator->lastPage() - 1)
+                    <p class="cont-puntos">. . .</p>
+                @endif
+
+                {{-- Última Página --}}
+                @if ($paginator->lastPage() > 1)
+                    @if ($paginator->currentPage() == $paginator->lastPage())
+                        <p class="cont-actual">{{ $paginator->lastPage() }}</p>
+                    @else
+                        <a href="{{ $paginator->url($paginator->lastPage()) }}" class="cont-link">{{ $paginator->lastPage() }}</a>
+                    @endif
+                @endif
+            </div>
         </div>
     </nav>
 @endif
 
 <style>
-    /* Un poco de estilo básico para que no salga todo pegado */
     .Paginacion-Navegacion {
         text-align: center;
         margin-top: 20px;
+        color: #0A2E36; 
     }
-    .Paginacion-Botones {
+
+    .Paginacion-Botones, .Contador-Flex {
         display: flex;
         justify-content: center;
         align-items: center;
         gap: 15px;
+    }
+
+    .Paginacion-Botones p, .Paginacion-Contador p {
+        margin: 0;
+        padding: 0;
+    }
+
+    .Separador-Vacio {
+        flex-grow: 1;
+    }
+
+    .btn-pag-link, .btn-pag-disabled, .cont-link, .cont-actual {
         font-weight: bold;
-        margin-bottom: 10px;
-        color: #0A2E36
+        font-size: 1.2rem;
     }
-    .btn-pag-link {
+
+    .btn-pag-link, .cont-link, .cont-puntos {
         color: #0A2E36;
-        text-decoration: none;
     }
+
+    .cont-actual {
+        color: #0A2E36;
+        border-bottom: 3px solid #0A2E36; 
+        padding: 0 5px;
+    }
+
     .btn-pag-disabled {
-        color: #cbd5e0;
+        color: #4A4A4A;
     }
-    .Paginacion-Contador {
-        font-size: 0.9rem;
-        color: #0A2E36;
+
+    /* --- MODO OSCURO --- */
+    body.dark-mode .Paginacion-Navegacion,
+    body.dark-mode .btn-pag-link,
+    body.dark-mode .cont-link,
+    body.dark-mode .cont-puntos,
+    body.dark-mode .cont-actual {
+        color: #1CAACA !important;
     }
+
+    body.dark-mode .cont-actual {
+        border-bottom-color: #1CAACA !important;
+    }
+
+    body.dark-mode .btn-pag-disabled {
+        color: #D9D9D9;
+    }
+
+    .cont-link { text-decoration: underline; }
+    .cont-puntos { letter-spacing: 3px; }
 </style>
